d4c05435efd9433e7718e30f447eca2b8e9c1c8e
Min <wyes91622@gmail.com>
2022-10-29 02:25:47 +0800

    add getWorkId

--- crawler/worklog.go
+++ crawler/worklog.go
@@ -27,3 +27,25 @@ type WorkLogCrawler struct {
 func (crawler *WorkLogCrawler) loginWorkStudy() {
 	crawler.Client.Get(WORKLOG_LOGIN)
 }
+
+func (crawler *WorkLogCrawler) getWorkId() (workId string) {
+	url := fmt.Sprintf("%s?date=%d/%d", WORKLOG_LIST_URL, crawler.StartTime.Year(), int(crawler.StartTime.Month()))
+	resp, err := crawler.Client.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	doc := soup.HTMLParse(string(body))
+	workRowList := doc.Find("tbody").FindAll("tr")
+	for i := 0; i < len(workRowList); i++ {
+		workRow := workRowList[i].FindAll("td")
+		if strings.Trim(workRow[2].Text(), " ") == crawler.WorkName {
+			workhref := workRow[8].Find("a").Attrs()["href"]
+			var rgx = regexp.MustCompile(`ContractId=\d+`)
+			rs := rgx.FindStringSubmatch(workhref)
+			workId = strings.Split(rs[0], "=")[1]
+		}
+	}
+	return
+}
