40baf80e03c763b65f8be5d2648b6fa740683b9c
Min <wyes91622@gmail.com>
2022-10-29 02:24:56 +0800

    add loginWorkStudy

--- crawler/worklog.go
+++ crawler/worklog.go
@@ -1,8 +1,17 @@
 package crawler
 
-import "time"
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/anaskhan96/soup"
+)
 
 const (
+	WORKLOG_LOGIN      = "https://webapp.yuntech.edu.tw/workstudy/Home/Login"
 	WORKLOG_LIST_URL   = "https://webapp.yuntech.edu.tw/workstudy/StudWorkRecord/ContractList"
 	WORKLOG_CREATE_URL = "https://webapp.yuntech.edu.tw/workstudy/StudWorkRecord/ApplyAction"
 )
@@ -13,4 +22,8 @@ type WorkLogCrawler struct {
 	StartTime   time.Time
 	EndTime     time.Time
 	WorkContent string
+}
+
+func (crawler *WorkLogCrawler) loginWorkStudy() {
+	crawler.Client.Get(WORKLOG_LOGIN)
 }
